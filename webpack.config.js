7909d4c9fd426549c386b2571f449de44746b790
TechieNg <melissacinta@gmail.com>
2021-11-16 15:16:39 +0100

    finished setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 const glob = require('glob');
 const {CleanWebpackPlugin} = require("clean-webpack-plugin");
 const PurgecssPlugin = require("purgecss-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
 
 let mode = "development";
